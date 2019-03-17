66d128e94de42199a379cd65ab76f4520f759eed
fenice <danielhan@Daniels-MacBook-Air.local>
2019-03-16 20:48:57 -0400

    i think its ok

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+var friends = [
+  {
+  name:"John Cena",
+  photo:"https://upload.wikimedia.org/wikipedia/commons/6/60/John_Cena_July_2018.jpg",
+  scores:[
+      '1',
+      '2',
+      '3',
+      '4',
+      '1',
+      '2',
+      '3',
+      '4',
+      '1',
+      '2'
+    ]
+ }
+];
+
+module.exports = friends;
